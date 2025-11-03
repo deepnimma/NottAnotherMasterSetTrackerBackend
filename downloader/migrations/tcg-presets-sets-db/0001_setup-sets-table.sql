ffdb11b2d25291b39191c883e498e11b43957e5b
Deepesh Nimma <deepnimma@gmail.com>
2025-11-03 17:02:56 -0500

    Create .sql file for second db

--- downloader/migrations/tcg-presets-sets-db/0001_setup-sets-table.sql
+++ downloader/migrations/tcg-presets-sets-db/0001_setup-sets-table.sql
@@ -1,1 +1,9 @@
 -- Migration number: 0001 	 2025-11-03T21:55:46.178Z
+CREATE TABLE presets_metadata (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    presetName TEXT NOT NULL,
+    images TEXT, -- JSON List of image links goes here
+
+    UNIQUE (presetName)
+);
