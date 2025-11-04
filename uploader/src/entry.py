f11ccb0a3b6aa5eca739a64171cde3172a7ba646
Deepesh Nimma <deepnimma@gmail.com>
2025-11-03 19:53:08 -0500

    testing

--- uploader/src/entry.py
+++ uploader/src/entry.py
@@ -1,5 +1,6 @@
 from workers import Response, WorkerEntrypoint
 from submodule import get_hello_message
+
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
-        return Response(get_hello_message())
+        return Response(self.env.UPLOADER_SECRET)
