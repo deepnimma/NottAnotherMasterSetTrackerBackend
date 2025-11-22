ffa75e4420cbef41d75479b0f14cbd1b417a071d
Deepesh Nimma <deepnimma@gmail.com>
2025-11-21 21:31:35 -0500

    remove image keys from body

--- downloader/src/database.py
+++ downloader/src/database.py
@@ -71,7 +71,6 @@ async def handle_request(request: Request, db) -> Response:
         "illustrator_flag": illustrator_flag,
         "descending_flag": descending,
         "num_found": len(image_keys),
-        "image_keys": image_keys,
         "image_rows": rows,
     }
 
